02502371bbca06341538e7195432352378b9e6bf
NucciTheBoss <nucci.programming@gmail.com>
2020-11-24 13:31:11 -0500

    Changed command to mv

--- shell_scripts/swap_container.sh
+++ shell_scripts/swap_container.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [[ -d ${HOME}/work/sw/turbo_seti && -x ${HOME}/work/sw/turbo_seti/turbo_seti_latest.sif ]]; then
-    rm -rf ${HOME}/work/sw/turbo_seti/turbo_seti_latest.sif
-    cp turbo_seti_new.sif ${HOME}/work/sw/turbo_seti/turbo_seti_latest.sif
+    mv turbo_seti_new.sif ${HOME}/work/sw/turbo_seti/turbo_seti_latest.sif
 
 else
     echo "Current turboSETI container does not exist. No need for swap."
-    
+
 fi
